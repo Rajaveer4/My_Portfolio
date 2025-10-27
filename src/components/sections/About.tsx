69ec547beebb73a75ed9903baefe040d80b12d12
Rajaveer Patil <rajveer.patil.rr@gmail.com>
2025-10-27 23:05:17 +0530

    Update profile image source in About section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -48,7 +48,7 @@ const About = () => {
               <div className="w-64 h-64 mx-auto lg:mx-0 rounded-full bg-gradient-primary p-1">
                 <div className="w-full h-full rounded-full overflow-hidden">
                   <img 
-                    src="https://res.cloudinary.com/dtjjgiitl/image/upload/q_auto:good,f_auto,fl_progressive/v1752093310/tiiormgyj4triiplcpgq.jpg" 
+                    src="/profile-image.jpg" 
                     alt="Rajaveer Patil"
                     className="w-full h-full object-cover"
                   />
